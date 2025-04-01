8887d094b35e6b09a2d65c15afc2b0b5347209e2
Tobechukwu-Njoku <tobechukwu.njoku@pm.me>
2025-04-01 13:38:54 +0100

    Temp files for new edits & display table of info

--- cache_small_files.sh
+++ cache_small_files.sh
@@ -9,32 +9,64 @@ CACHE_NAME="cache"                                  # Set the name of your cache
 SIZE_LIMIT="256M"                                   # Set size limit (e.g., 10M for 10MB)
 DRY_RUN=false                                       # Set to true for a dry run
 
+# Declare environment variables
 PATH_SHARE_ARRAY="/mnt/user0/$SHARE_NAME"           # Source (only from array)
 PATH_SHARE_CACHE="/mnt/$CACHE_NAME/$SHARE_NAME"     # Destination (cache disk)
 
-# Count files under $SIZE_LIMIT before processing
-echo "Counting files under $SIZE_LIMIT in $SHARE_NAME"
-file_count_array=$(find $PATH_SHARE_ARRAY -type f -size $SIZE_LIMIT | wc -l)
-file_count_cache=$(find $PATH_SHARE_CACHE -type f -size $SIZE_LIMIT | wc -l)
-echo "$SHARE_NAME Array: $file_count_array files under $SIZE_LIMIT"
-echo "$SHARE_NAME Cache: $file_count_cache files under $SIZE_LIMIT"
+# Declare arrays to store results
+declare -A file_count_array
+declare -A file_count_cache
+size_thresholds=()
 
-# Display table to visualize level of efficacy for each level of file size
-file_count_array_sub8m=$(find $PATH_SHARE_ARRAY -type f -size -8M | wc -l)
-file_count_cache_sub8m=$(find $PATH_SHARE_CACHE -type f -size -8M | wc -l)
-file_count_array_sub16m=$(find $PATH_SHARE_ARRAY -type f -size +8M -size -16M | wc -l)          # Count files between size parameters
-file_count_cache_sub16m=$(find $PATH_SHARE_CACHE -type f -size +8M -size -16M | wc -l)
-file_count_array_sub32m=$(find $PATH_SHARE_ARRAY -type f -size +16M -size -32M | wc -l)
-file_count_cache_sub32m=$(find $PATH_SHARE_CACHE -type f -size +16M -size -32M | wc -l)
-file_count_array_super32m=$(find $PATH_SHARE_ARRAY -type f -size +32M | wc -l)
-file_count_cache_super32m=$(find $PATH_SHARE_CACHE -type f -size +32M | wc -l)
+# Generate size thresholds dynamically
+size_thresholds[0]=$START_SIZE
+for ((i = 1; i < ITERATIONS; i++)); do
+    prev_size=${size_thresholds[$((i-1))]}
+    next_size=$(echo "$prev_size * $SIZE_MULTIPLIER" | bc)M
+    size_thresholds[i]=$next_size
+done
+
+# Count files for each size range
+for ((i = 0; i < ITERATIONS; i++)); do
+    lower_size=${size_thresholds[$i]}
+
+    if [[ $i -eq 0 ]]; then
+        # First range: Files smaller than START_SIZE
+        file_count_array["sub$lower_size"]=$(find "$PATH_SHARE_ARRAY" -type f -size -"$lower_size" | wc -l)
+        file_count_cache["sub$lower_size"]=$(find "$PATH_SHARE_CACHE" -type f -size -"$lower_size" | wc -l)
+    else
+        upper_size=${size_thresholds[$i]}
+        file_count_array["sub$upper_size"]=$(find "$PATH_SHARE_ARRAY" -type f -size +"$lower_size" -size -"$upper_size" | wc -l)
+        file_count_cache["sub$upper_size"]=$(find "$PATH_SHARE_CACHE" -type f -size +"$lower_size" -size -"$upper_size" | wc -l)
+    fi
+done
+
+# Count files larger than the last threshold
+final_threshold=${size_thresholds[$((ITERATIONS-1))]}
+file_count_array["super$final_threshold"]=$(find "$PATH_SHARE_ARRAY" -type f -size +"$final_threshold" | wc -l)
+file_count_cache["super$final_threshold"]=$(find "$PATH_SHARE_CACHE" -type f -size +"$final_threshold" | wc -l)
+
+# Display results in table format
 echo "-------------------------------------"
-printf "| %-4s | %-10s | %-10s | %-10s |\n" "Size" "Array" "Cache: $CACHE_NAME" "Total"
+printf "| %-6s | %-10s | %-10s | %-10s |\n" "Size" "Array" "Cache: $CACHE_NAME" "Total"
 echo "-------------------------------------"
-printf "| %-4s | %-10s | %-10s | %-10s |\n" "<8M" "$file_count_array_sub8m" "$file_count_cache_sub8m"
-printf "| %-4s | %-10s | %-10s | %-10s |\n" "<16M" "$file_count_array_sub16m" "$file_count_cache_sub16m"
-printf "| %-4s | %-10s | %-10s | %-10s |\n" "<32M" "$file_count_array_sub32m" "$file_count_cache_sub32m"
-printf "| %-4s | %-10s | %-10s | %-10s |\n" ">32M" "$file_count_array_super32m" "$file_count_cache_super32m"
+
+for ((i = 0; i < ITERATIONS; i++)); do
+    size_label="<${size_thresholds[$i]}"
+    array_count=${file_count_array["sub${size_thresholds[$i]}"]}
+    cache_count=${file_count_cache["sub${size_thresholds[$i]}"]}
+    total_count=$((array_count + cache_count))
+
+    printf "| %-6s | %-10s | %-10s | %-10s |\n" "$size_label" "$array_count" "$cache_count" "$total_count"
+done
+
+# Print final row for files larger than the last threshold
+size_label=">${final_threshold}"
+array_count=${file_count_array["super$final_threshold"]}
+cache_count=${file_count_cache["super$final_threshold"]}
+total_count=$((array_count + cache_count))
+
+printf "| %-6s | %-10s | %-10s | %-10s |\n" "$size_label" "$array_count" "$cache_count" "$total_count"
 echo "-------------------------------------"
 
 # Function to move files while preserving directory structure
@@ -48,11 +80,11 @@ move_files() {
 
     if [ "$DRY_RUN" = true ]; then
         # This uses a lot of CPU time, only uncomment temporarily for detailed information
-        #echo "Would Move: $src_file -> $dest_path"
+        echo "Would Move: $src_file -> $dest_path"
     else
         mv "$src_file" "$dest_path"
         # This uses a lot of CPU time, only uncomment temporarily for detailed information
-        #echo "Moved: $src_file -> $dest_path"
+        echo "Moved: $src_file -> $dest_path"
     fi
 }
 
