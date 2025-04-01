187eb1888ba6919590c801b7d028a4a4b622e0b9
Tobechukwu-Njoku <tobechukwu.njoku@pm.me>
2025-04-01 10:57:18 +0100

    Display table, removed path logging

--- cache_small_files.sh
+++ cache_small_files.sh
@@ -6,7 +6,7 @@
 # Configuration
 SHARE_NAME="share_name"                             # Set the name of your share here
 CACHE_NAME="cache"                                  # Set the name of your cache here
-SIZE_LIMIT="256M"                                    # Set size limit (e.g., 10M for 10MB)
+SIZE_LIMIT="256M"                                   # Set size limit (e.g., 10M for 10MB)
 DRY_RUN=false                                       # Set to true for a dry run
 
 PATH_SHARE_ARRAY="/mnt/user0/$SHARE_NAME"           # Source (only from array)
@@ -19,6 +19,24 @@ file_count_cache=$(find $PATH_SHARE_CACHE -type f -size $SIZE_LIMIT | wc -l)
 echo "$SHARE_NAME Array: $file_count_array files under $SIZE_LIMIT"
 echo "$SHARE_NAME Cache: $file_count_cache files under $SIZE_LIMIT"
 
+# Display table
+file_count_array_sub8m=$(find $PATH_SHARE_ARRAY -type f -size -8M | wc -l)
+file_count_cache_sub8m=$(find $PATH_SHARE_CACHE -type f -size -8M | wc -l)
+file_count_array_sub16m=$(find $PATH_SHARE_ARRAY -type f -size +8M -size -16M | wc -l)
+file_count_cache_sub16m=$(find $PATH_SHARE_CACHE -type f -size +8M -size -16M | wc -l)
+file_count_array_sub32m=$(find $PATH_SHARE_ARRAY -type f -size +16M -size -32M | wc -l)
+file_count_cache_sub32m=$(find $PATH_SHARE_CACHE -type f -size +16M -size -32M | wc -l)
+file_count_array_super32m=$(find $PATH_SHARE_ARRAY -type f -size +32M | wc -l)
+file_count_cache_super32m=$(find $PATH_SHARE_CACHE -type f -size +32M | wc -l)
+echo "-----------------------------"
+printf "| %-4s | %-10s | %-10s |\n" "Size" "Array" "Cache: $CACHE_NAME"
+echo "-----------------------------"
+printf "| %-4s | %-10s | %-10s |\n" "<8M" "$file_count_array_sub8m" "$file_count_cache_sub8m"
+printf "| %-4s | %-10s | %-10s |\n" "<16M" "$file_count_array_sub16m" "$file_count_cache_sub16m"
+printf "| %-4s | %-10s | %-10s |\n" "<32M" "$file_count_array_sub32m" "$file_count_cache_sub32m"
+printf "| %-4s | %-10s | %-10s |\n" ">32M" "$file_count_array_super32m" "$file_count_cache_super32m"
+echo "-----------------------------"
+
 # Function to move files while preserving directory structure
 move_files() {
     local src_file="$1"
@@ -29,10 +47,12 @@ move_files() {
     mkdir -p "$(dirname "$dest_path")"
 
     if [ "$DRY_RUN" = true ]; then
-        echo "Would Move: $src_file -> $dest_path"
+        # This uses a lot of CPU time, only uncomment temporarily for detailed information
+        #echo "Would Move: $src_file -> $dest_path"
     else
         mv "$src_file" "$dest_path"
-        echo "Moved: $src_file -> $dest_path"
+        # This uses a lot of CPU time, only uncomment temporarily for detailed information
+        #echo "Moved: $src_file -> $dest_path"
     fi
 }
 
