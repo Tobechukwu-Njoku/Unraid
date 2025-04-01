d2e2a71631d0235b2966f8bc9aaef949a66f6461
Tobechukwu-Njoku <tobechukwu.njoku@pm.me>
2025-04-01 11:26:13 +0100

    Comment changes

--- cache_small_files.sh
+++ cache_small_files.sh
@@ -19,23 +19,23 @@ file_count_cache=$(find $PATH_SHARE_CACHE -type f -size $SIZE_LIMIT | wc -l)
 echo "$SHARE_NAME Array: $file_count_array files under $SIZE_LIMIT"
 echo "$SHARE_NAME Cache: $file_count_cache files under $SIZE_LIMIT"
 
-# Display table
+# Display table to visualize level of efficacy for each level of file size
 file_count_array_sub8m=$(find $PATH_SHARE_ARRAY -type f -size -8M | wc -l)
 file_count_cache_sub8m=$(find $PATH_SHARE_CACHE -type f -size -8M | wc -l)
-file_count_array_sub16m=$(find $PATH_SHARE_ARRAY -type f -size +8M -size -16M | wc -l)
+file_count_array_sub16m=$(find $PATH_SHARE_ARRAY -type f -size +8M -size -16M | wc -l)          # Count files between size parameters
 file_count_cache_sub16m=$(find $PATH_SHARE_CACHE -type f -size +8M -size -16M | wc -l)
 file_count_array_sub32m=$(find $PATH_SHARE_ARRAY -type f -size +16M -size -32M | wc -l)
 file_count_cache_sub32m=$(find $PATH_SHARE_CACHE -type f -size +16M -size -32M | wc -l)
 file_count_array_super32m=$(find $PATH_SHARE_ARRAY -type f -size +32M | wc -l)
 file_count_cache_super32m=$(find $PATH_SHARE_CACHE -type f -size +32M | wc -l)
-echo "-----------------------------"
-printf "| %-4s | %-10s | %-10s |\n" "Size" "Array" "Cache: $CACHE_NAME"
-echo "-----------------------------"
-printf "| %-4s | %-10s | %-10s |\n" "<8M" "$file_count_array_sub8m" "$file_count_cache_sub8m"
-printf "| %-4s | %-10s | %-10s |\n" "<16M" "$file_count_array_sub16m" "$file_count_cache_sub16m"
-printf "| %-4s | %-10s | %-10s |\n" "<32M" "$file_count_array_sub32m" "$file_count_cache_sub32m"
-printf "| %-4s | %-10s | %-10s |\n" ">32M" "$file_count_array_super32m" "$file_count_cache_super32m"
-echo "-----------------------------"
+echo "-------------------------------------"
+printf "| %-4s | %-10s | %-10s | %-10s |\n" "Size" "Array" "Cache: $CACHE_NAME" "Total"
+echo "-------------------------------------"
+printf "| %-4s | %-10s | %-10s | %-10s |\n" "<8M" "$file_count_array_sub8m" "$file_count_cache_sub8m"
+printf "| %-4s | %-10s | %-10s | %-10s |\n" "<16M" "$file_count_array_sub16m" "$file_count_cache_sub16m"
+printf "| %-4s | %-10s | %-10s | %-10s |\n" "<32M" "$file_count_array_sub32m" "$file_count_cache_sub32m"
+printf "| %-4s | %-10s | %-10s | %-10s |\n" ">32M" "$file_count_array_super32m" "$file_count_cache_super32m"
+echo "-------------------------------------"
 
 # Function to move files while preserving directory structure
 move_files() {
